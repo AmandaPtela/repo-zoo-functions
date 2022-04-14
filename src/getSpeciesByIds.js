6875fb6312ae1df512eeb4cb1a50d536a3af84eb
Amanda Portela <mandaptela@gmail.com>
2022-04-14 17:04:22 -0300

    nada ainda

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  const especie = () => data.species.filter(item => data.species.includes(ids));
-  return(especie());
+const getSpeciesByIds = (ids) => {const especie = () => data.species.filter(item => data.species.includes(ids));
+  console.log(especie(ids));
 }
+getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46');
 
 module.exports = getSpeciesByIds;
