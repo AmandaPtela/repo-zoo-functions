6492c61e392b8b62bf9c3411983b7e663e02a0ed
Amanda Portela <mandaptela@gmail.com>
2022-04-16 16:18:15 -0300

    iniciando função

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,17 +2,16 @@ const data = require('../data/zoo_data');
 
 const objeto = data.species;
 const animals = Object.values(objeto);
-let i = 0;
 
-const getAnimals = (nome) => {
-  const arrayAnimais = animals.forEach(animals => animals.name);
-  console.log(arrayAnimais);}
-  /* if(arrayAnimais.values === nome){
-    console.log(arrayAnimais);
+const getAnimals = (nome) => {animals.forEach(i => {
+  if (i.name === nome){
+    console.log({name: nome} , i.residents.length);
   }
-  undefined;
-} */
-getAnimals('lions')
+  undefined
+});
+}
+
+getAnimals('lions');
 
 //const countAnimals = () => {}
 //const arrayItens = (nome) => objeto.forEach(nomess => {console.log({name:nome}.includes(nomess))});
