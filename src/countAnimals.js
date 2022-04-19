d18d7a7675ea684d77a5bd0a0cb3ef2b586b519a
Amanda Portela <mandaptela@gmail.com>
2022-04-18 21:48:24 -0300

    req 1

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,15 +3,15 @@ const data = require('../data/zoo_data');
 const objeto = data.species;
 const animals = Object.values(objeto);
 
-const getAnimals = (nome) => {animals.forEach(i => {
-  if (i.name === nome){
-    console.log({name: nome} , i.residents.length);
+const getAnimals = (nome , sex) => {animals.forEach(i => {
+  if ({name} === nome && i.residents[1] === sex){
+    console.log(i.residents.length);
   }
   undefined
 });
 }
 
-getAnimals('lions');
+getAnimals({specie:'penguins'} , {sex:'female'});
 
 //const countAnimals = () => {}
 //const arrayItens = (nome) => objeto.forEach(nomess => {console.log({name:nome}.includes(nomess))});
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,10 +2,8 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 
-const getSpeciesByIds = (ids) => {
-    const especie = species.forEach(element => {
-        console.log(especie);
-    });
+const getSpeciesByIds = (...ids) => {
+    return ids ? data.species.filter((specie) => ids.includes(specie.id)) : [];
 }
 getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
 
