6e975a900e2bb156d569fc4d00713b1277688ae9
Amanda Portela <mandaptela@gmail.com>
2022-04-23 17:38:28 -0300

    ipdate 3

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,39 +1,39 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-//contagem por especie
-/* function countAnimalsBySpecie (especie) { species.forEach(element => { 
-  if (element.name == Object.values(especie)){
-  console.log(element.residents.length)}});}
-countAnimalsBySpecie({specie: 'tigers'}); */
-
-//contagem por especie e sexo
-/* const countAnimalsBySex = (sexo) => { species.forEach(element => {
-  function sexoAnimal () { element.residents.forEach((sex) => sex === Object.keys(sexo))}
-  const filtrado = species.filter(sexoAnimal);
-  console.log(filtrado)})};
-countAnimalsBySex({sex: 'female'}); */
-
-//caso não haja parametro
-  /* if (especie.length = 0){ 
-    species.forEach(element => { console.log(element.name + ' ' + element.residents.length)})} */
-
-//const countAnimals = ;
 function countAnimalsBySpecie (especie) {
   let especieAchada = species.find((item) => item.name === especie);
   return especieAchada.residents.length;
 }
-countAnimalsBySpecie('tigers');
+//countAnimalsBySpecie('tigers');
 
 //find by sex
 function countAnimalsBySex (animal, sexo) {
   let animalEncontrado = species.find((item) => item.name === animal);
   let arrayAnimais = animalEncontrado.residents.filter((item) => item.sex === sexo);
-  console.log(arrayAnimais.length);
+  return(arrayAnimais.length);
 }
-countAnimalsBySex('lions', 'male');
+//countAnimalsBySex('lions', 'male');
 
 function countEverything () {
-  let 
+  let reduzido = species.reduce((acumulador, item) => {
+    acumulador[item.name] = countAnimalsBySpecie(item.name);
+    return acumulador;
+  } , {})
+  return reduzido;
 }
-//module.exports = countAnimals;
+//countEverything();
+
+function countAnimals(object) {
+  if (!object) {
+    return (countEverything());
+  }
+  else if (object.specie && object.sex){
+    return (countAnimalsBySex(object.specie, object.sex));
+  }
+  else {
+    return (countAnimalsBySpecie(object.specie));
+  }
+}
+countAnimals({ specie: 'penguins' , sex: 'male' })
+module.exports = countAnimals;
