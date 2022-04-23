457eb702045606ef49e254cdb259c5ccb03b137f
Amanda Portela <mandaptela@gmail.com>
2022-04-23 19:02:13 -0300

    update

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,11 +1,11 @@
-/* const { species } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const po = [];
+
 function getAnimalsOlderThan(animal, age) {
   const maisVelhos = data.species.filter(age => data.species.age >= 10);
   console.log(po.push(maisVelhos));
 }
 getAnimalsOlderThan('lion', 10);
 
-module.exports = getAnimalsOlderThan; */
+module.exports = getAnimalsOlderThan;
