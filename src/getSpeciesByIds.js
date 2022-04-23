31b8ba6aebbe1b21bc46e2bf4df272f3ae01859d
Amanda Portela <mandaptela@gmail.com>
2022-04-23 18:59:20 -0300

    getSpeciesById quase ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,6 +1,14 @@
-/* const { species } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = () => {
+const getSpeciesByIds = (id) => {
+    const ids = species.find((item) => item.id === id);
+    if (id){
+        console.log(ids)
+    }
+    if (!id){
+        console.log([]);
+    }
 }
-module.exports = getSpeciesByIds; */
+getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46');
+module.exports = getSpeciesByIds;
