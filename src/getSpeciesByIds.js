eb4c009ca440b4972bf07f73dbbc5b4b152e9642
Amanda Portela <mandaptela@gmail.com>
2022-04-25 17:22:44 -0300

    format animalsById

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,13 +2,13 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (id) => {
-const ids = species.find((item) => item.id === id);
-	if (id) {
-	  return ([ids]);
-	}
-	if (!id) {
-	  return ([]);
-}
+	const ids = species.find((item) => item.id === id);
+  if (id) {
+    return ([ids]);
+  }
+  if (!id) {
+    return ([]);
+  }
 };
-getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d', 'e8481c1d-42ea-4610-8e11-1752cfc05a46');
+getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d');
 module.exports = getSpeciesByIds;
