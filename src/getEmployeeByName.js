98b4ffbefe9cfcea2a0277c90a3d2ac6ae8b7875
Amanda Portela <mandaptela@gmail.com>
2022-04-25 17:20:31 -0300

    format employees

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,10 +2,10 @@ const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-if (!employeeName) {
-  return ({ });
-}
-return employees.find((i) => i.firstName === employeeName || i.lastName === employeeName);
+	if (!employeeName) {
+  	return ({ });
+	}
+	return employees.find((i) => i.firstName === employeeName || i.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
