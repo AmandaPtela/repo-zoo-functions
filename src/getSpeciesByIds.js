47a41c800bee61da64efa302d0a7bda36ef8183d
Amanda Portela <mandaptela@gmail.com>
2022-04-25 17:12:01 -0300

    format

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -9,6 +9,6 @@ const getSpeciesByIds = (id) => {
     if (!id) {
         return([]);
     }
-}
-getSpeciesByIds();
+};
+getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d', 'e8481c1d-42ea-4610-8e11-1752cfc05a46');
 module.exports = getSpeciesByIds;
