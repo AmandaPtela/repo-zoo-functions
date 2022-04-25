83fd648539721d862d1a5d333a8626a49ef48bc3
Amanda Portela <mandaptela@gmail.com>
2022-04-25 17:55:20 -0300

    OlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,13 +3,7 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   const animais = species.find((item) => item.name === animal);
-  const idade = animais.residents.filter((item) => item.age >= age);
-  for (let i = 0; i <= 10; i += 1) {
-    if (idade[i].age > age) {
-      return true;
-    }
-    return false;
-  }
+  return animais.residents.every((item) => item.age >= age);
 }
 getAnimalsOlderThan('otters', 7);
 
