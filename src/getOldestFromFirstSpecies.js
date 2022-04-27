3dc7a5074c8b1c06317e94b5274a92b4c9c3df12
Amanda Portela <mandaptela@gmail.com>
2022-04-27 18:11:38 -0300

    correção

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -4,10 +4,10 @@ const data = require('../data/zoo_data');
 function getOldestFromFirstSpecies(id) {
   // recebendo id do funcionário, retorna a primeira especie de bicho que ele cuidou. dps filtra o mais velho desses.}
   const funcionariosId = employees.find((item) => item.id === id);
-  const responsavelPor = funcionariosId.responsibleFor;
+  const responsavelPor = funcionariosId.responsibleFor[0];
   const animais = data.species.filter((item) => responsavelPor.includes(item.id));
-  const filtro = animais.forEach((item) => console.log(item.residents.every((item) => item.age)));
-  console.log(idade);
+  const filtro = animais.forEach((item) => (item.residents.every((item) => item.age)));
+  console.log(responsavelPor);
 }
 getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
 module.exports = getOldestFromFirstSpecies;
