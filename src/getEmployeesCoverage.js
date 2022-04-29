b23c2a2e6765ceeda4ed89a4f06bbacec2ee9081
Amanda Portela <mandaptela@gmail.com>
2022-04-29 13:43:05 -0300

    update

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,15 +2,16 @@ const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeesCoverage(nome) {
-/*   const funcionario = employees.find((item) => item.firstName === Object.values(nome) || item.lastName === Object.values(nome));
-   if (!nome) {
-      return {employees};
-    }
-    if (Object.values(nome) || Object.values(nome)){
-      return funcionario;
-    };
+  const funcionario = employees.find((item) =>
+    item.firstName === Object.values(nome) || item.lastName === Object.values(nome));
+  if (!nome) {
+    return { employees };
+  }
+  if (Object.values(nome)) {
+    return funcionario;
+  }
   return funcionario;
 }
-getEmployeesCoverage({name: 'Burl'}) */
-}
+getEmployeesCoverage({ name: 'Burl' });
+
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,15 +1,14 @@
-const { employees, species } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // recebendo id do funcionário, retorna a primeira especie de bicho que ele cuida. dps filtra o mais velho desses.}
   const funcionariosId = employees.find((item) => item.id === id);
   const responsavelPor = funcionariosId.responsibleFor[0];
-  const animal = species.find((item) => item.id === responsavelPor);
+  const animal = data.species.find((item) => item.id === responsavelPor);
   const maisVelho = animal.residents.sort((a, b) => b.age - a.age);
   return Object.values(maisVelho[0]);
-
-
 }
+
 getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f');
 module.exports = getOldestFromFirstSpecies;
