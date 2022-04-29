20bc9c047f9bcb1bc4952582f79175b148b06fb5
Amanda Portela <mandaptela@gmail.com>
2022-04-29 17:06:33 -0300

    fazer getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,11 @@
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  // Sem parametro, retorna Horario de funcionamento e animais do dia
+  // parâmetros não seja um animal e dia, retorna objeto com os horários do dia e os animais em exibição'
+  // ' com "Monday" , informar que o zoológico está fechado'
+  // com animal , array com os dias q ele ta disponivel
+  // com dia, retorna horario de funcionamento e os animais do dia
 }
-
 module.exports = getSchedule;
