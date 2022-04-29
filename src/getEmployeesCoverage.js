a9c1812c394d78df6f5522162ef116f9e2dac0fd
Amanda Portela <mandaptela@gmail.com>
2022-04-29 19:12:26 -0300

    4/5

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,11 +2,15 @@ const {species , employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeesCoverage(object) {
+  const funcionario = employees.find((item) => item.firstName === object.name || item.lastName == object.name || item.id === object.id);
+  if (!funcionario) {
+    throw new Error('Informações inválidas');
+  }
 
+  const animais = species.filter((item) => {if (funcionario.responsibleFor.includes(item.id)){ return item.name}});
+  const localizacao = species.filter((item) => funcionario.responsibleFor.includes(item.id));
+  
   if(object.name || object.id) {
-    const funcionario = employees.find((item) => item.firstName === object.name || item.lastName == object.name || item.id === object.id);
-    const animais = species.filter((item) => {if (funcionario.responsibleFor.includes(item.id)){ return item.name}});
-    const localizacao = species.filter((item) => funcionario.responsibleFor.includes(item.id));  
       return {
       id: funcionario.id ,
       fullName: `${funcionario.firstName} ${funcionario.lastName}`,
@@ -14,22 +18,9 @@ function getEmployeesCoverage(object) {
       locations: localizacao.map((item)=> item.location) ,
     };
   }
-
-  if (typeof funcionario === 'undefined') {
-    throw new Error('Informações inválidas')
-  };
-
+  // sem param
   if (typeof object === 'undefined') {
-  const funcionarios = employees.find((item) => item.firstName);
-  const animais = species.filter((item) => {if (funcionarios.responsibleFor.includes(item.id)){ return item.name}});
-  const localizacao = species.filter((item) => funcionarios.responsibleFor.includes(item.id));  
- 
-    return {
-    id: funcionarios.id ,
-    fullName: `${funcionarios.firstName} ${funcionarios.lastName}` ,
-    species: animais.map((item) => item.name),
-    locations: localizacao.map((item)=> item.location) ,
-  };
+
   }
 }
 module.exports = getEmployeesCoverage;
