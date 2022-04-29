d36f2b292709b27a9567ff13a65165623ab8d00e
Amanda Portela <mandaptela@gmail.com>
2022-04-29 13:28:11 -0300

    falta ordenar idade

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,14 +1,15 @@
-const { employees } = require('../data/zoo_data');
+const { employees , species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // recebendo id do funcionário, retorna a primeira especie de bicho que ele cuidou. dps filtra o mais velho desses.}
+  // recebendo id do funcionário, retorna a primeira especie de bicho que ele cuida. dps filtra o mais velho desses.}
   const funcionariosId = employees.find((item) => item.id === id);
   const responsavelPor = funcionariosId.responsibleFor[0];
-  const animais = data.species.filter((item) => responsavelPor === item.id).residents;
-  //const residentes = animais.residents.filter((item) => item.age > 10);
+  const animal = species.find((item) => item.id === responsavelPor);
+  const maisVelho = animal.residents.filter((item) => item.age > 14);
+  const array = Object.values(maisVelho[0])
+console.log(array);
 
-  console.log(animais);
 }
 getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
 module.exports = getOldestFromFirstSpecies;
