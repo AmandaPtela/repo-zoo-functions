83adafb5b48fa67ccbf5b8f3393d7b9d752fd428
Amanda Portela <mandaptela@gmail.com>
2022-04-29 15:50:33 -0300

    format

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -3,22 +3,22 @@ const data = require('../data/zoo_data');
 
 function isManager(idGerente) {
   const gerente = employees.find((item) => item.managers.includes(idGerente));
-  if (gerente){
+  if (gerente) {
     return true;
   }
-  if (!gerente){
+  if (!gerente) {
     return false;
   }
 }
 
 function getRelatedEmployees(managerId) {
-  if (isManager(managerId)){
-  const eGerente = isManager(managerId);
-  const gerente = employees.find((item) => item.id === managerId);
-  const funcionarios = employees.filter((item) => item.managers.includes(gerente.id));
-  let arrayNomes = [];
-  const nomes = funcionarios.filter((item) => arrayNomes.push(item.firstName + ' ' + item.lastName));
-  return arrayNomes;
+  if (isManager(managerId)) {
+    const gerente = employees.find((item) => item.id === managerId);
+    const funcionarios = employees.filter((item) => item.managers.includes(gerente.id));
+    const arrayNomes = [];
+    const nomes = funcionarios.filter((i) => arrayNomes.push(`${i.firstName} ${i.lastName}`));
+    console.log(nomes);
+    return arrayNomes;
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
