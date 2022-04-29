458620d4646c0182c9c10a1b68b66ffad01721f1
Amanda Portela <mandaptela@gmail.com>
2022-04-29 18:55:45 -0300

    coverage 3/5

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,27 +2,14 @@ const {species , employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeesCoverage(object) {
-  if (!object){
-    const todos = employees.forEach((item) => {
-     const funcionario = employees.find((item) => item.firstName || item.lastName === object.name);
-    const animais = species.filter((item) => {if (funcionario.responsibleFor.includes(item.id)){ return item.name}});
-    const localizacao = species.filter((item) => funcionario.responsibleFor.includes(item.id));  
-      return {
-      id: funcionario.id ,
-      fullName: `${funcionario.firstName} ${funcionario.lastName}` ,
-      species: animais.map((item) => item.name),
-      locations: localizacao.map((item)=> item.location) ,
-    };
-  })
-}
+
   if(object.name) {
-    const funcionario = employees.find((item) => item.firstName || item.lastName === object.name);
-    console.log(funcionario.firstName);
+    const funcionario = employees.find((item) => item.firstName === object.name || item.lastName == object.name);
     const animais = species.filter((item) => {if (funcionario.responsibleFor.includes(item.id)){ return item.name}});
     const localizacao = species.filter((item) => funcionario.responsibleFor.includes(item.id));  
       return {
       id: funcionario.id ,
-      fullName: `${funcionario.firstName} ${funcionario.lastName}` ,
+      fullName: `${funcionario.firstName} ${funcionario.lastName}`,
       species: animais.map((item) => item.name),
       locations: localizacao.map((item)=> item.location) ,
     };
@@ -31,15 +18,30 @@ function getEmployeesCoverage(object) {
     const funcionario = employees.find((item) => item.id === object.id);
     const animais = species.filter((item) => {if (funcionario.responsibleFor.includes(item.id)){ return item.name}});
     const localizacao = species.filter((item) => funcionario.responsibleFor.includes(item.id));  
-    /* console.log(`${funcionario.firstName} ${funcionario.lastName}`); */
       return {
       id: funcionario.id ,
       fullName: `${funcionario.firstName} ${funcionario.lastName}`,
       species: animais.map((item) => item.name),
       locations: localizacao.map((item)=> item.location) ,
     };
-
+    
   }
-  throw new Error('Informações inválidas');
+
+  if (typeof funcionario === 'undefined') {
+    throw new Error('Informações inválidas')
+  };
+
+  if (typeof object === 'undefined') {
+  const funcionarios = employees.find((item) => item.firstName === object.name || item.lastName === object.name);
+  const animais = species.filter((item) => {if (funcionarios.responsibleFor.includes(item.id)){ return item.name}});
+  const localizacao = species.filter((item) => funcionarios.responsibleFor.includes(item.id));  
+ 
+    return {
+    id: funcionarios.id ,
+    fullName: `${funcionarios.firstName} ${funcionarios.lastName}` ,
+    species: animais.map((item) => item.name),
+    locations: localizacao.map((item)=> item.location) ,
+  };
+  }
 }
 module.exports = getEmployeesCoverage;
