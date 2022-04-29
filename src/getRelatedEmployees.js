7cd30262bb305b9f1b888156a32603f98a4a6862
Amanda Portela <mandaptela@gmail.com>
2022-04-29 15:45:46 -0300

    relatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,26 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
+function isManager(idGerente) {
+  const gerente = employees.find((item) => item.managers.includes(idGerente));
+  if (gerente){
+    return true;
+  }
+  if (!gerente){
+    return false;
+  }
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)){
+  const eGerente = isManager(managerId);
+  const gerente = employees.find((item) => item.id === managerId);
+  const funcionarios = employees.filter((item) => item.managers.includes(gerente.id));
+  let arrayNomes = [];
+  const nomes = funcionarios.filter((item) => arrayNomes.push(item.firstName + ' ' + item.lastName));
+  return arrayNomes;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
