b766ca24e0f80575273ec0649e3eaa7d8ea49a47
Amanda Portela <mandaptela@gmail.com>
2022-04-29 22:08:18 -0300

    format

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,48 +2,35 @@ const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const pegarNome = (id) => species.find((specie) => specie.id === id).name;
-const localidade  = (id) => species.find((specie) => specie.id === id).location;
+const localidade = (id) => species.find((specie) => specie.id === id).location;
 
-function semParametro () {
+function semParametro() {
   const array = [];
   employees.forEach((cada) => {
     array.push({
-      id: cada.id ,
-      fullName: `${cada.firstName} ${cada.lastName}` ,
-      species: cada.responsibleFor.map((animal) => pegarNome(animal)) ,
-      locations: cada.responsibleFor.map((local) => localidade(local)) ,
-    })
-  })
-  return array;
+      id: cada.id,
+      fullName: `${cada.firstName} ${cada.lastName}`,
+      species: cada.responsibleFor.map((animal) => pegarNome(animal)),
+      locations: cada.responsibleFor.map((local) => localidade(local)),
+    });
+  }); return array;
 }
 
 function getEmployeesCoverage(obj) {
-  // sem parametro
-  if (typeof obj === 'undefined') {
-    return semParametro();
-  }
-  
-  const funcionario = employees.find((trbalhador) =>
-  trbalhador.firstName === obj.name || trbalhador.lastName === obj.name || trbalhador.id === obj.id);
+  if (typeof obj === 'undefined') { return semParametro(); }
+  const funcionario = employees.find((worker) =>
+    worker.firstName === obj.name || worker.lastName === obj.name || worker.id === obj.id);
   if (typeof funcionario === 'undefined') {
     throw new Error('Informações inválidas');
   }
-
-  const animais = species.filter((item) => {
-    if (funcionario.responsibleFor.includes(item.id)) {
-      return item.name;
-    }
-  });
+  const an = species.filter((i) => funcionario.responsibleFor.includes(i.id)).filter((o) => o.name);
   const localizacao = species.filter((item) =>
     funcionario.responsibleFor.includes(item.id));
-
-  if (obj.name || obj.id) {
-    return {
-      id: funcionario.id,
-      fullName: `${funcionario.firstName} ${funcionario.lastName}`,
-      species: animais.map((item) => item.name),
-      locations: localizacao.map((item) => item.location),
-    };
-  }
+  return {
+    id: funcionario.id,
+    fullName: `${funcionario.firstName} ${funcionario.lastName}`,
+    species: an.map((item) => item.name),
+    locations: localizacao.map((item) => item.location),
+  };
 }
 module.exports = getEmployeesCoverage;
