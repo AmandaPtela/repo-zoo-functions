8cf94bbaca30071c3d55c3daa22005aa196d8d09
Amanda Portela <mandaptela@gmail.com>
2022-05-03 18:30:57 -0300

    Schedule

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,68 +1,66 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function semParam () {
-  return {
+/*   return {
     NE: species.filter((specie) => specie.location === 'NE').map((animal) => animal.name),
     NW: species.filter((specie) => specie.location === 'NW').map((animal) => animal.name),
     SE: species.filter((specie) => specie.location === 'SE').map((animal) => animal.name),
     SW: species.filter((specie) => specie.location === 'SW').map((animal) => animal.name),
   }
 }
-//console.log(semParam());
-function animaisPoNome (animal) {
-  return species.filter((item) => item.name === animal).reduce((acc , item) => { 
-    acc[item.name] = item.residents.map((item)=> item.name); 
-    return acc; 
-    } , {} );
-}
-//console.log(animaisPoNome('tigers'));
 
+function getAnimaslsResidents() {
+  const animais = species.filter
+}
+//console.log(semParam());
 function getAnimalMap(options) {
   const animaisNE = species.filter(({ location }) => location === 'NE').filter(({residents}) => residents).map((item) => item.name);
   const animaisNW = species.filter(({ location }) => location === 'NW').filter(({residents}) => residents).map((item) => item.name);
   const animaisSE = species.filter(({ location }) => location === 'SE').filter(({residents}) => residents).map((item) => item.name); 
   const animaisSW = species.filter(({ location }) => location === 'SW').filter(({residents}) => residents).map((item) => item.name);
-  const residentes = species.filter((specie) => specie.location === 'NE');
-
-  
   const semParametro = semParam();
+
+  const animalEncontrado = species.find((item) => animaisNE.includes(item.name));
+  const arrayAnimais = animalEncontrado.residents.map(
+    (item) => item.name);
+
+  console.log(arrayAnimais);
   if (!options) {
     return semParametro;
   }
+
   if(!options.includesName) {
     return semParametro;
   }
 
   if (options.includesName) {
-    //const residentes = animaisPoNome('lions , giraffes');
     return {
       NE: species.filter((item) => animaisNE.includes(item.name)).reduce((acc , item) => { 
         const obj = {};
-        obj[item.name] = residentes.filter((item)=>item.name); 
+        obj[item.name] = arrayAnimais[0]; 
         acc.push(obj);
         return acc; 
         } , [] ),
       NW: species.filter((item) => animaisNW.includes(item.name)).reduce((acc , item) => { 
         const obj = {};
-        obj[item.name] = residentes; 
+        obj[item.name] = '2'; 
         acc.push(obj);
         return acc; 
         } , [] ),
       SE: species.filter((item) => animaisSE.includes(item.name)).reduce((acc , item) => { 
         const obj = {};
-        obj[item.name] = residentes; 
+        obj[item.name] = '2'; 
         acc.push(obj);
         return acc; 
         } , [] ),
       SW: species.filter((item) => animaisSW.includes(item.name)).reduce((acc , item) => { 
         const obj = {};
-        obj[item.name] = residentes; 
+        obj[item.name] = '2'; 
         acc.push(obj);
         return acc; 
         } , [] ),
     }
-  }
+  } */
 
 /*   if(options.sorted){
     return {
@@ -85,5 +83,4 @@ function getAnimalMap(options) {
     }
   } */
 }
-console.log(getAnimalMap({includesName: true}));
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -3,20 +3,27 @@ const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
   // Sem parametro, retorna Horario de funcionamento e animais do dia // tudo
+  if (!scheduleTarget) {
+  const {open , close} = hours[scheduleTarget];
+  const animais = species.filter((item) => item.availability.includes(scheduleTarget)).map((item) => item.name);
+  return {[scheduleTarget]: {
+    'officeHour': `Open from ${open} until ${close}` ,
+    'exhibition': animais ,
+  }}};
+}
   // parâmetros não seja um animal e dia, retorna a mesma coisa ^
   // ' com "Monday" , informar que o zoológico está fechado'
 
   // com animal , array com os dias q ele ta disponivel
-/*   if (scheduleTarget){
+/*    if (scheduleTarget){
   const animais = species.filter((item) => item.name === scheduleTarget).map((item) => item.availability);
   console.log(animais);
-  } */
+  }  */
 
   // com dia, retorna horario de funcionamento e os animais do dia
 /*   if (scheduleTarget) {
     const dias = hours.day;
     console.log(dias);
   } */
-}
-getSchedule('Sunday');
+console.log(getSchedule('Sunday'));
 module.exports = getSchedule;
